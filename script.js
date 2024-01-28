67f185c65fff68b0f53a98ad13f7e633b2656e8b
SanskarSinghRajput <you@example.com>
2024-01-28 22:21:31 +0530

    "Initial commit"

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ var typeData = new Typed(".role", {
 function sendEmail() {
     window.location.href = "mailto:sanskarsingh9999@gmail.com";
 }
+
+document.getElementById("readMoreBtn1").addEventListener("click", function() {
+    window.open('https://github.com/SanskarSinghRajput/E-Commerce-Web-Application', '_blank');
+});
+
+document.getElementById("readMoreBtn2").addEventListener("click", function() {
+  window.open('https://github.com/SanskarSinghRajput/Nike-Store-E-commerce-website', '_blank');
+});
+
+document.getElementById("readMoreBtn3").addEventListener("click", function() {
+  window.open('https://github.com/SanskarSinghRajput/Social-media-Frontend', '_blank');
+});
+
+document.getElementById("readMoreBtn4").addEventListener("click", function() {
+  window.open('https://github.com/SanskarSinghRajput/RestrauntWesite', '_blank');
+});
+
+document.getElementById("readMoreBtn5").addEventListener("click", function() {
+  window.open('https://github.com/SanskarSinghRajput/WEEKAWAY-TourPlanner-Website', '_blank');
+});
